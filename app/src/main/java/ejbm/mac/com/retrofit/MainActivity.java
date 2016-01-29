9f4b72b60534a3de024bb2ef8b9a55eecc149e5a
Esteban Briones <esteban.brionesmac@gmail.com>
2016-01-28 21:27:04 -0500

    Initial commit

---
+++ app/src/main/java/ejbm/mac/com/retrofit/MainActivity.java
@@ -0,0 +1,25 @@
+package ejbm.mac.com.retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById( R.id.tv1) ;
+
+
+    }
+
+    public void getCall(View view) {
+
+    }
+}
